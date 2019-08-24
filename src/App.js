2b8a53a937bd2fda00bbfa988a5c315667f2ba98
amnonbb <amnonbb@gmail.com>
2019-08-24 13:12:39 +0300

    TrlAdmin: fix root permission content

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { Component, Fragment } from 'react';
 import 'semantic-ui-css/semantic.min.css';
 // import TrlApp from "./apps/App";
-import TrlChat from "./apps/Chat/App";
+// import TrlChat from "./apps/Chat/App";
 // import TrlClient from "./apps/Client/App";
-// import TrlAdmin from "./apps/Admin/App";
+import TrlAdmin from "./apps/Admin/App";
 
 class App extends Component {
 
@@ -14,9 +14,9 @@ class App extends Component {
         return (
             <Fragment>
                 {/*<TrlApp />*/}
-                <TrlChat />
+                {/*<TrlChat />*/}
                 {/*<TrlClient/>*/}
-                {/*<TrlAdmin/>*/}
+                <TrlAdmin/>
             </Fragment>
 
         );
