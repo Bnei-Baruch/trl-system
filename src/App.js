e5d049122a9907fa3d77bd618f7e5636a4fb89ef
amnonbb <amnonbb@gmail.com>
2022-03-12 11:45:19 +0200

    Make new we client mqtt and audiobridge based

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import React, { Component, Fragment } from 'react';
 import 'semantic-ui-css/semantic.min.css';
 // import Main from "./apps/Main";
-import MqttClient from "./apps/Client/MqttClient";
+// import MqttClient from "./apps/Client/MqttClient";
 // import MqttAdmin from "./apps/Admin/MqttAdmin";
 // import HttpClient from "./apps/Client/HttpClient";
 // import HttpAdmin from "./apps/Admin/HttpAdmin";
 // import WeApp from "./apps/WeApp";
-// import WeClient from "./apps/WeClient/App";
-import WeLive from "./apps/WeLive/App";
-// import TrlApp from "./apps/App";
-// import TrlChat from "./apps/Chat/App";
-// import TrlClient from "./apps/Client/App";
-// import TrlAdmin from "./apps/Admin/App";
+import WeClient from "./apps/Client/WeClient";
+// import WeLive from "./apps/Client/App";
 
 class App extends Component {
 
@@ -22,17 +18,13 @@ class App extends Component {
         return (
             <Fragment>
                 {/*<Main />*/}
-                <MqttClient />
+                {/*<MqttClient />*/}
                 {/*<MqttAdmin />*/}
                 {/*<HttpClient />*/}
                 {/*<HttpAdmin />*/}
-                {/*<TrlApp />*/}
-                {/*<TrlChat />*/}
-                {/*<TrlClient/>*/}
-                {/*<TrlAdmin />*/}
                 {/*<WeApp />*/}
-                {/*<WeClient />*/}
-                <WeLive />
+                <WeClient />
+                {/*<WeLive />*/}
             </Fragment>
 
         );
