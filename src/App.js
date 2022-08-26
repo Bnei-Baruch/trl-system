61675024ae22d81bd7de21e226e91476ea03a126
amnonbb <amnonbb@gmail.com>
2022-08-26 12:26:26 +0300

    TrlChat: refactor code for mqtt transport

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component, Fragment } from 'react';
 import 'semantic-ui-css/semantic.min.css';
 // import Main from "./apps/Main";
-import MqttClient from "./apps/Client/MqttClient";
+// import MqttClient from "./apps/Client/MqttClient";
 // import HttpClient from "./apps/Client/HttpClient";
 // import MqttAdmin from "./apps/Admin/MqttAdmin";
 // import HttpAdmin from "./apps/Admin/HttpAdmin";
+import TrlChat from "./apps/Chat/TrlChat";
 // import WeMain from "./apps/WeMain";
 // import WeClient from "./apps/Client/WeClient";
 // import WeStream from "./apps/Stream/WeStream";
@@ -18,10 +19,11 @@ class App extends Component {
         return (
             <Fragment>
                 {/*<Main />*/}
-                <MqttClient />
+                {/*<MqttClient />*/}
                 {/*<HttpClient />*/}
                 {/*<MqttAdmin />*/}
                 {/*<HttpAdmin />*/}
+                <TrlChat />
                 {/*<WeMain />*/}
                 {/*<WeClient />*/}
                 {/*<WeStream />*/}
