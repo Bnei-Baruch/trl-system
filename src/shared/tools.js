50bd38b8a2d0b2cc9da4fb0c20a49a58f156d5e2
amnonbb <amnonbb@gmail.com>
2024-07-06 13:09:26 +0300

    Change audio notification function

--- src/shared/tools.js
+++ src/shared/tools.js
@@ -362,17 +362,21 @@ const stereoVisualizer = (analyser1, analyser2, canvas, width, n) => {
     p[n] = setInterval(sampleAudioStream, 50);
 };
 
-const beepAudio = (freq = 660, duration = 90, vol = 5) => {
-    let context = new(window.AudioContext || window.webkitAudioContext);
-    const oscillator = context.createOscillator();
-    const gain = context.createGain();
-    gain.gain.setValueAtTime(0, context.currentTime);
-    gain.gain.linearRampToValueAtTime(1, context.currentTime + 0.002);
-    oscillator.connect(gain);
-    oscillator.frequency.value = freq;
-    oscillator.type = "square";
-    gain.connect(context.destination);
-    oscillator.start(context.currentTime);
-    oscillator.stop(context.currentTime + duration * .001);
-    oscillator.onended = () => context.close();
+const beepAudio = () => {
+    let audioCtx = new(window.AudioContext || window.webkitAudioContext);
+
+    const oscillator = audioCtx.createOscillator();
+    const gainNode = audioCtx.createGain();
+
+    oscillator.connect(gainNode);
+    gainNode.connect(audioCtx.destination);
+
+    gainNode.gain.value = 0.1;
+    oscillator.frequency.value = 500;
+    oscillator.type = 'sine';
+
+    oscillator.start();
+    setTimeout(() => {
+        oscillator.stop();
+    }, 500);
 }
